1938f13d47a80691d854d3783cf8ffc6ac0e19c2
Callum James Lodge <callumjlodge@gmail.com>
2025-10-22 15:30:00 +0100

    found out issue is due to redirecting to current site, trying reset() instead

--- src/app/error.js
+++ src/app/error.js
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 
-export default function ErrorPage() {
+export default function ErrorPage({ reset }) {
   return (
     <>
       <h1>An error has occured!</h1>
@@ -9,9 +9,9 @@ export default function ErrorPage() {
         You may not have been logged in, and need to do so in order to make
         posts, vote on posts or most other things on the site.
         <br />
-        Please Sign in or Sign up from the main site
+        Please Sign in or Sign up from the button on the top right
       </p>
-      <Link href={"/"}>Return to main page</Link>
+      <button onClick={() => reset()}>Return to previous page</button>
     </>
   );
 }
