578349cbd4d4a89d5047db2439ef8c50394c4ce6
Callum James Lodge <callumjlodge@gmail.com>
2025-10-22 15:02:13 +0100

    troubleshooting

--- src/app/error.js
+++ src/app/error.js
@@ -7,8 +7,8 @@ export default function ErrorPage() {
     <>
       <h1>An error has occured!</h1>
       <p>
-        You may not have been logged in, and need to do so in order to vote on
-        posts.
+        You may not have been logged in, and need to do so in order to make
+        posts, vote on posts or most other things on the site.
         <br />
         Please Sign in or Sign up from the main site
       </p>
